a4b0d3b99d002bd7a85f1c0194fbc21767a8b43c
Matheus Medeiros <mathmed22@gmail.com>
2022-05-18 15:55:24 -0300

    Primeiro exercicio

--- Program.java
+++ Program.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Peça um número e valide se este número é divisível por 5.
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		int numero = sc.nextInt();
+		if(numero % 5 == 0) {
+			System.out.println("O número é divisível por 5");
+		} else {
+			System.out.println("Não é divisível por 5");
+		}
+		
+		sc.close();
 	}
 
 }
