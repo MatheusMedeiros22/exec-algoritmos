fb457dafe7236d4a4575039f725080ba08472fa2
Matheus Medeiros <mathmed22@gmail.com>
2022-05-22 16:39:25 -0300

    exercicio-do-dia letscode

--- Program.java
+++ Program.java
@@ -6,27 +6,41 @@ public class Program {
 	public static void main(String[] args) {
 
 		/*
-		 * Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo
-		 * que calcule seu peso ideal, utilizando as seguintes fórmulas: • Para homens:
-		 * (72.7h) - 58 • Para mulheres: (62.1h) - 44.7
+		 * Faça um Programa que pergunte o quanto você ganha por hora e o número de
+		 * horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido
+		 * mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o
+		 * INSS e 5% para o sindicato.
+		 * 
+		 * O programa deve nos informar:
+		 * 
+		 * A. salário bruto. B. quanto pagou ao INSS. C. quanto pagou ao sindicato. D. o
+		 * salário líquido.
+		 * 
+		 * Calcule os descontos e o salário líquido, conforme a tabela abaixo:
+		 * 
+		 * + Salário Bruto : R$ - IR (11%) : R$ - INSS (8%) : R$ - Sindicato ( 5%) : R$
+		 * = Salário Liquido : R$
+		 * 
+		 * Obs.: Salário Bruto - Descontos = Salário Líquido.
 		 */
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Informe seu gênero (m,f) ");
-		char genero = sc.next().charAt(0);
-		System.out.println("Digite a sua altura: ");
-		double altura = sc.nextDouble();
 		
-		double pesoIdeal = 0.0;
+		System.out.println("How much do you earn per hour?");
+		double earnPerHour = sc.nextDouble();
+		System.out.println("How much hours work per month");
+		int hoursWorkPerMonth = sc.nextInt();
 		
-		if(genero == 'm') {
-			pesoIdeal += (72.7 * altura) - 58;
-		}else {
-			pesoIdeal += (62.1 * altura) - 44.7;
-		}
+		double salarioBruto = earnPerHour * hoursWorkPerMonth;
+		double inss = salarioBruto * 0.08;
+		double sindicato = salarioBruto * 0.11;
+		double salarioLiquido = salarioBruto - inss - sindicato;
 		
-		System.out.println("Seu peso ideal é = " + String.format("%.2f", pesoIdeal) + "kg");
+		System.out.println("Salario Bruto: R$ " + String.format("%.2f", salarioBruto));
+		System.out.println("INSS: R$ " + String.format("%.2f", inss));
+		System.out.println("Sindicato: R$ " + String.format("%.2f", sindicato));
+		System.out.println("Salario Liquido: R$ " + String.format("%.2f", salarioLiquido));
 		
 
 		sc.close();
