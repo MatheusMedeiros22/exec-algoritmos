479e67e04ead28eb34b800b667cac0c675ff1206
Matheus Medeiros <mathmed22@gmail.com>
2022-05-22 16:27:43 -0300

    exercicio-do-dia letscode

--- Program.java
+++ Program.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
@@ -5,48 +6,29 @@ public class Program {
 	public static void main(String[] args) {
 
 		/*
-		 * Suponha que uma pessoa possa se aposentar pelo INSS caso atenda alguma das
-		 * situações abaixo: • É do gênero masculino, possui pelo menos 65 anos e pelo
-		 * menos 10 anos de contribuição. • É do gênero masculino, possui pelo menos 63
-		 * anos e pelo menos 15 anos de contribuição. • É do gênero feminino, possui
-		 * pelo menos 63 anos e pelo menos 10 anos de contribuição. • É do gênero
-		 * feminino, possui pelo menos 61 anos e pelo menos 15 anos de contribuição.
-		 * Crie um programa que leia um caractere (‘M’ ou ‘F’), que representa o gênero
-		 * de um indivíduo, e dois inteiros, que representam a idade e o tempo de
-		 * contribuição. O programa deverá então imprimir “Aposentável” se o indivíduo
-		 * atender uma das situações acima. Caso contrário, o programa deverá imprimir
-		 * “Não Aposentável”
+		 * Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo
+		 * que calcule seu peso ideal, utilizando as seguintes fórmulas: • Para homens:
+		 * (72.7h) - 58 • Para mulheres: (62.1h) - 44.7
 		 */
-
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Digite o gênero do indivíduo ('m'/'f'): ");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe seu gênero (m,f) ");
 		char genero = sc.next().charAt(0);
-		System.out.println("Digite a idade:");
-		int idade = sc.nextInt();
-		System.out.println("Digite o tempo de contribuição:");
-		int tempoContribuicao = sc.nextInt();
+		System.out.println("Digite a sua altura: ");
+		double altura = sc.nextDouble();
+		
+		double pesoIdeal = 0.0;
 		
 		if(genero == 'm') {
-			if(idade >= 65 && tempoContribuicao >= 10) {
-				System.out.println("Aposentavel");
-			}else if(idade >= 63 && tempoContribuicao >= 15){
-				System.out.println("Aposentavel");
-			}else {
-				System.out.println("Não aposentavel");
-			}
+			pesoIdeal += (72.7 * altura) - 58;
+		}else {
+			pesoIdeal += (62.1 * altura) - 44.7;
 		}
 		
-		if(genero == 'f') {
-			if(idade >= 63 && tempoContribuicao >= 10) {
-				System.out.println("Aposentavel");
-			}else if(idade >= 61 && tempoContribuicao >= 15){
-				System.out.println("Aposentavel");
-			}else {
-				System.out.println("Não aposentavel");
-			}
-		}
+		System.out.println("Seu peso ideal é = " + String.format("%.2f", pesoIdeal) + "kg");
 		
+
 		sc.close();
 	}
 
